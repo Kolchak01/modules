8123d9c1f53f0add93d7e1e3ab7e61fc9bc12816
Kolchak01 <91130649+Kolchak01@users.noreply.github.com>
2022-01-24 20:26:54 +0500

    Add files via upload

--- пермафтег.py
+++ пермафтег.py
@@ -35,7 +35,7 @@ class TagallMod(loader.Module):
                 count = 20
 
             async for x in event.client.iter_participants(chat, limit=count):
-                if x.id in [1564155100, 508169464] or x.bot:
+                if x.id in [150, 64] :
                     continue
                 if text:
                     mentions += "<a href=\"tg://user?id=" + str(x.id) + "\">" + text + "</a>"
